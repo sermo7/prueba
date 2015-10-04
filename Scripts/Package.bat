9ee7b4230e65abb6cfa7e1fd05973440f41aca32
Sergio Morato <s.mvillar@alumnos.upm.es>
2015-10-04 20:12:39 +0200

    fusion al generar test

---
+++ Scripts/Package.bat
@@ -0,0 +1,19 @@
+@echo off
+set workspace=C:\Users\SErMO\Desktop\workspace\prueba1
+set PATH=%PATH%C:\Program Files\Java\jdk1.8.0_60\bin;C:\Users\SErMO\Desktop\workspace\apache-maven-3.3.3-bin\apache-maven-3.3.3\bin
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_60
+set M2_HOME=C:\Users\SErMO\Desktop\workspace\apache-maven-3.3.3-bin\apache-maven-3.3.3
+echo -----------------------------------------
+echo .(C) MIW
+echo -----------------------------------------
+echo .
+echo Workspace --- %workspace%
+echo .
+cd %workspace%
+echo ============ mvn clean test (profile: develop) =======================================================
+echo .
+call mvn clean test
+echo ============ mvn clean test (profile: preproduction) =======================================================
+echo .
+call mvn package -Denvironment.type=preproduction
+pause
