3610b6383b03fc1ad7f3f582ce58b35220764d5d
Sergio Morato <s.mvillar@alumnos.upm.es>
2015-10-04 13:52:53 +0200

    crear C11

---
+++ scripts/Test.bat
@@ -0,0 +1,16 @@
+@echo off
+set workspace=C:\Users\SErMO\Desktop\workspace\prueba
+set PATH=%PATH%C:\Program Files\Java\jdk1.8.0_60\bin;C:\Users\SErMO\Desktop\workspace\apache-maven-3.3.3-bin\apache-maven-3.3.3\bin
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_60
+set M2_HOME=C:\Users\SErMO\Desktop\workspace\apache-maven-3.3.3-bin\apache-maven-3.3.3
+echo -----------------------------------------
+echo . (C) MIW
+echo -----------------------------------------
+echo .
+echo Workspace --- %workspace%
+echo .
+cd %workspace%
+echo ============ mvn clean test (profile: develop) =======================================================
+echo .
+call mvn clean test
+pause
---
+++ src/main/java/prueba/C11.java
@@ -0,0 +1,8 @@
+package prueba;
+
+public class C11 {
+
+    public String m1(){
+        return "m1";
+    }
+}
